60e37cd37c56c41b4a93ae55b1992904b2bfe70d
Aram-Osipyan <osipyan.aram2017@ya.ru>
2020-07-09 16:14:57 +0300

    сделал песочницу (172)

---
+++ Game/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    [SerializeField] float chaseRange = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var len = Vector3.Distance(gameObject.transform.position , target.position);
+
+        if (len < chaseRange)
+            navMeshAgent.SetDestination(target.position);
+        //else
+            //navMeshAgent.SetDestination(gameObject.transform.position);
+    }
+}
